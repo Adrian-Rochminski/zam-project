f851e05a2d4f33fa41dfa439ecded79aca35a674
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-10-28 16:46:59 +0200

    feat: fixed session dependency, added session configuration and altered organizer entity

--- src/main/java/com/studies/zamproject/entities/Organizer.java
+++ src/main/java/com/studies/zamproject/entities/Organizer.java
@@ -4,11 +4,15 @@ package com.studies.zamproject.entities;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Table(name = "organizer")
-public class Organizer {
+public class Organizer implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,6 +29,49 @@ public class Organizer {
     @Size(max = 20)
     private String telephone;
 
+    @NotNull
+    @Size(max = 256)
+    private String role;
+
+    @NotNull
+    @Size(max = 20)
+    private String password;
+
     @OneToMany(mappedBy = "organizer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<Event> events;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
