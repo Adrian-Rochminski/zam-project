a3de233b05b007c538c947f616f2009b6a502aa6
Krizzuu <47757676+Krizzuu@users.noreply.github.com>
2023-10-29 18:13:53 +0100

    implemented two methods of event service

--- src/main/java/com/studies/zamproject/services/EventService.java
+++ src/main/java/com/studies/zamproject/services/EventService.java
@@ -1,6 +1,8 @@
 package com.studies.zamproject.services;
 
+import com.studies.zamproject.dtos.EventRequestDTO;
 import com.studies.zamproject.entities.Event;
+import com.studies.zamproject.mappers.EventMapper;
 import com.studies.zamproject.repositories.EventRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,15 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepo;
+    private final EventMapper eventMapper;
 
-    public Event addEvent(Event eventRequest) {
-        return null;
+    public Event addEvent(EventRequestDTO eventRequest) {
+        Event event = eventMapper.eventReqDtoToEvent(eventRequest);
+        return eventRepo.save(event);
     }
 
-    public Optional<Event> getEvent(Event eventRequest) {
-        return Optional.empty();
+    public Optional<Event> getEvent(Long eventId) {
+        return eventRepo.findById(eventId);
     }
 
     public Event updateEvent(Event eventRequest) {
