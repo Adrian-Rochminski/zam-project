acb453164ffb456b1ebef3e4a055143ac23a884f
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-10-29 19:21:01 +0100

    fix(User): mapped by fix

--- src/main/java/com/studies/zamproject/entities/Event.java
+++ src/main/java/com/studies/zamproject/entities/Event.java
@@ -4,10 +4,9 @@ package com.studies.zamproject.entities;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
 import java.util.Set;
 
 @Entity
--- src/main/java/com/studies/zamproject/entities/User.java
+++ src/main/java/com/studies/zamproject/entities/User.java
@@ -52,7 +52,7 @@ public class User implements UserDetails {
     @Setter
     private String password;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @Getter
     @Setter
     private Set<Event> events;
