b94d3f80a64c5cddb539dc50542893c76f13fa5e
Krizzuu <47757676+Krizzuu@users.noreply.github.com>
2023-10-29 17:29:20 +0100

    added EventRequestDTO

---
+++ src/main/java/com/studies/zamproject/dtos/EventRequestDTO.java
@@ -0,0 +1,33 @@
+package com.studies.zamproject.dtos;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class EventRequestDTO {
+    @NotBlank
+    @Size(max = 256)
+    private String name;
+
+    private Boolean free;
+
+    @Size(max = 10000)
+    private String description;
+
+    @DecimalMin("-90.0")
+    @DecimalMax("90.0")
+    private Double latitude;
+
+    @DecimalMin("-180.0")
+    @DecimalMax("180.0")
+    private Double longitude;
+
+    private Set<String> tags;
+}
