f661ba6a63f9abeb92bb1053740473fed8a766c0
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-11-01 16:13:26 +0100

    feat: added validation exception handler

---
+++ src/main/java/com/studies/zamproject/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+/* (C)2023 */
+package com.studies.zamproject.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldError().getDefaultMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
