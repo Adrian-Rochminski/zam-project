91a024214aa2d5b4adc8c2c8642fde2349841463
Adrian-Rochminski <235959@edu.p.lodz.pl>
2023-11-09 00:03:44 +0100

    added communication between front and back

---
+++ src/main/java/com/studies/zamproject/configuration/CorsGlobalConfiguration.java
@@ -0,0 +1,24 @@
+package com.studies.zamproject.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobalConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://192.168.0.100:8081")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
