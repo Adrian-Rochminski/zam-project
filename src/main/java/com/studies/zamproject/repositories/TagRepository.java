7e27ea7684369d27a4737a21b68a7b01ba121114
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-12-11 20:54:36 +0100

    fix: dodanie tagów do domyślnych eventów oraz zmiana dat

--- src/main/java/com/studies/zamproject/repositories/TagRepository.java
+++ src/main/java/com/studies/zamproject/repositories/TagRepository.java
@@ -4,4 +4,6 @@ package com.studies.zamproject.repositories;
 import com.studies.zamproject.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Tag, Long> {}
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Tag findByName(String name);
+}
